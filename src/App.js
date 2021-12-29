b57ea73420de5aba96bb47b8db6dd95a08ab92b7
Derrik Campbell <derrikcampbell@hotmail.com>
2021-12-29 00:21:03 -0500

    Setting up firebase for authentication backend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <h1>Login authentication with React and Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
